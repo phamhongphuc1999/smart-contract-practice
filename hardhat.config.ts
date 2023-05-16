2eb9acad840684e3104ac3d2c69de698dc2f4388
phamhongphuc1999 <phamhongphuc12atm1@gmail.com>
2023-05-16 09:57:29 +0700

    feat: test and deploy code

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,12 +1,22 @@
-import "@nomicfoundation/hardhat-toolbox";
-import { HardhatUserConfig } from "hardhat/config";
-import "@typechain/hardhat";
+import '@nomicfoundation/hardhat-toolbox';
+import { HardhatUserConfig } from 'hardhat/config';
+import '@typechain/hardhat';
+import 'hardhat-prettier';
+
+const INFURA_API_KEY = 'KEY';
+const SEPOLIA_PRIVATE_KEY = 'YOUR SEPOLIA PRIVATE KEY';
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.18",
+  solidity: '0.8.18',
   typechain: {
-    outDir: "typechain",
-    target: "ethers-v5",
+    outDir: 'typechain',
+    target: 'ethers-v5',
+  },
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [SEPOLIA_PRIVATE_KEY],
+    },
   },
 };
 
---
+++ scripts/deploy.ts
@@ -0,0 +1,18 @@
+import { ethers } from 'hardhat';
+import { Token__factory } from '../typechain';
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+  console.log('Deploying contracts with the account:', deployer.address);
+  console.log('Account balance:', (await deployer.getBalance()).toString());
+  const ethersSigner = ethers.provider.getSigner();
+  const token = await new Token__factory(ethersSigner).deploy();
+  console.log('Token address: ', token.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error: any) => {
+    console.error(error);
+    process.exit(1);
+  });
