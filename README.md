20eee9b36332f133ac2a144f3f9105f4a20ea020
phamhongphuc1999 <phamhongphuc12atm1@gmail.com>
2024-03-13 10:05:11 +0700

    fix: verify proof

--- README.md
+++ README.md
@@ -30,3 +30,4 @@ yarn test
 - https://docs.soliditylang.org/en
 - https://solidity-by-example.org/app/multi-sig-wallet/
 - https://medium.com/@AyoAlfonso/implement-account-abstraction-aa-to-rotate-private-keys-while-maintaining-the-same-pubkey-address-b5c01f4d3cbb
+- https://vivianblog.hashnode.dev/how-to-create-a-zero-knowledge-dapp-from-zero-to-production#heading-6-test-circuits
--- test/verifier.test.ts
+++ test/verifier.test.ts
@@ -15,26 +15,25 @@ describe('Verifier', function () {
       const { verifier } = await loadFixture(deployVerifier);
       const isVerify = await verifier.verifyProof(
         [
-          '0x117e92179fbb5507dc68393072d91d007a5874568aeafc193b72ca593b3df358',
-          '0x2bd3856a7d4b3730fbefc1497533d91b724ae6b1d2e66cba4636ffe321a867ff',
+          '0x240d93248cdf13f86c41bd51157a218cdd480a5665a97adb686b848a05f50ce5',
+          '0x13a18df3388a51e07fb1af422a928d15b20ba471ff1c4f8624e66efaff71a6e1',
         ],
         [
           [
-            '0x1f1f9f29419b0ee4279f817ed5e0777963b1ee037e40e06707541b7b23e33df1',
-            '0x0860a1fbeaad5e0598cb578967e1cd513e6523b287facd88217abcd4e6dd1136',
+            '0x004ff263ab5b09c076c841cad47aabd36973a71c7554f6d1f2dd1d59c33b2710',
+            '0x2489ae0695995ac22266b4bf260cdf00ed5570b37f2762b46363faf7e4d7da7e',
           ],
           [
-            '0x1e97b41c7e38a04327cc4579ff5a84c954dcabbca1ae05c863dd3bf4c525633b',
-            '0x127745082e033c46700941843a526bd8a8d847675a7353b92cbfa96d0ac6c52f',
+            '0x05fbb551eae9447b34ff8a3bab00ed2196d1cafcccaad85cdb8059bc158a9497',
+            '0x305e35dae166a9b34318da2e52344b2310ce241332c80db7ed3f886026100a2e',
           ],
         ],
         [
-          '0x04e5f5c1974ab5953df33b2f5b54de014b1e6c5849785abf990d70fa877a0aee',
-          '0x0cf1dc3d1613f39abf53684dc428a7d49bfad7dae195fcfafcf02f179f2f182b',
+          '0x01985a5772e99285cc28973e049d6e4596d38604de2f5d0800c5b953137f33b5',
+          '0x27a707709948ccc52fc9f3ce0a6c2b410646d7bde6e66dd55b94269eb9c807f5',
         ],
-        ['0x000000000000000000000000000000000000000000000000000000000000001e']
+        ['0x0000000000000000000000000000000000000000000000000000000000000028']
       );
-      console.log('🚀 ~ isVerify:', isVerify);
       expect(isVerify).to.equal(true);
     });
   });
